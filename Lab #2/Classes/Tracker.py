8d160cef5b3cd0abbac9169b9423d606e32db53b
YourBestDream <ferari100w3@gmail.com>
2023-10-28 14:14:41 +0300

    I hapo that this will work for 9

--- Lab #2/Classes/Tracker.py
+++ Lab #2/Classes/Tracker.py
@@ -1,140 +1,121 @@
 import os
-import datetime
+import hashlib
+from datetime import datetime
 
-class FileSnapshot:
-    def __init__(self, filename, extension):
-        self.filename = filename
-        self.extension = extension
-        self.created_time = datetime.datetime.now()
-        self.updated_time = self.created_time
+class File:
+    def __init__(self, name, path):
+        self.name = name
+        self.path = path
+        self.last_snapshot_hash = self.calculate_hash()
+        self.snapshot_time = datetime.now()
 
-    def update_snapshot(self):
-        self.updated_time = datetime.datetime.now()
+    def calculate_hash(self):
+        sha256 = hashlib.sha256()
+        with open(self.path, 'rb') as file:
+            while True:
+                data = file.read(65536)  # Read in 64k chunks
+                if not data:
+                    break
+                sha256.update(data)
+        return sha256.hexdigest()
 
-class TextFileSnapshot(FileSnapshot):
-    def __init__(self, filename):
-        super().__init__(filename, "txt")
-        self.line_count = 0
-        self.word_count = 0
-        self.character_count = 0
+    def has_changed(self):
+        current_hash = self.calculate_hash()
+        return current_hash != self.last_snapshot_hash
 
-class ImageFileSnapshot(FileSnapshot):
-    def __init__(self, filename):
-        super().__init__(filename, "png")
-        self.image_size = (0, 0)
+    def get_file_info(self):
+        created_time = datetime.fromtimestamp(os.path.getctime(self.path))
+        updated_time = datetime.fromtimestamp(os.path.getmtime(self.path))
+        return f'{self.name} - Created: {created_time}, Updated: {updated_time}'
 
-class ProgramFileSnapshot(FileSnapshot):
-    def __init__(self, filename):
-        super().__init__(filename, "py")
-        self.line_count = 0
-        self.class_count = 0
-        self.method_count = 0
+    def get_status(self):
+        if self.has_changed():
+            return f'{self.name} - Changed'
+        else:
+            return f'{self.name} - No Change'
 
-class FileMonitor:
+
+class TextFile(File):
+    def get_file_info(self):
+        file_info = super().get_file_info()
+        with open(self.path, 'r') as text_file:
+            lines = text_file.readlines()
+            word_count = sum(len(line.split()) for line in lines)
+            char_count = sum(len(line) for line in lines)
+            return f'{file_info}, Lines: {len(lines)}, Words: {word_count}, Characters: {char_count}'
+
+
+class ImageFile(File):
+    def get_file_info(self):
+        file_info = super().get_file_info()
+        width, height = self.get_image_dimensions()
+        return f'{file_info}, Image Size: {width}x{height}'
+
+    def get_image_dimensions(self):
+        # You can implement this to extract image dimensions from image files (png, jpg).
+        return (0, 0)  # For example, return 0x0
+
+
+class ProgramFile(File):
+    def get_file_info(self):
+        file_info = super().get_file_info()
+        line_count, class_count, method_count = self.get_code_metrics()
+        return f'{file_info}, Lines: {line_count}, Classes: {class_count}, Methods: {method_count}'
+
+    def get_code_metrics(self):
+        # You can implement this to count lines, classes, and methods in program files (.py, .java).
+        return (0, 0, 0)  # For example, return (0, 0, 0)
+
+
+class FolderMonitor:
     def __init__(self, folder_path):
         self.folder_path = folder_path
-        self.file_snapshots = {}
-        self.snapshot_time = None
+        self.files = {}
 
     def commit(self):
-        self.snapshot_time = datetime.datetime.now()
-
-        for filename in os.listdir(self.folder_path):
-            if filename.endswith(".py"):
-                with open(os.path.join(self.folder_path, filename), "r") as file:
-                    lines = file.readlines()
-                    snapshot = FileSnapshot(filename, "py")  # Create a FileSnapshot instance
-                    snapshot.updated_time = datetime.datetime.now()
-                    snapshot.line_count = len(lines)
-                    snapshot.class_count = sum(1 for line in lines if "class" in line)
-                    snapshot.method_count = sum(1 for line in lines if "def" in line)
-                    self.file_snapshots[filename] = snapshot
+        for root, _, files in os.walk(self.folder_path):
+            for file in files:
+                file_path = os.path.join(root, file)
+                if file_path not in self.files:
+                    self.files[file_path] = File(file, file_path)
+                else:
+                    self.files[file_path].last_snapshot_hash = self.files[file_path].calculate_hash()
+                    self.files[file_path].snapshot_time = datetime.now()
 
     def info(self, filename):
-        if filename in self.file_snapshots:
-            file_snapshot = self.file_snapshots[filename]
-            print(f"{file_snapshot.filename}.{file_snapshot.extension}")
-            if file_snapshot.extension == "txt":
-                print(f"Line Count: {file_snapshot.line_count}")
-                print(f"Word Count: {file_snapshot.word_count}")
-                print(f"Character Count: {file_snapshot.character_count}")
-            elif file_snapshot.extension == "png":
-                print(f"Image Size: {file_snapshot.image_size[0]}x{file_snapshot.image_size[1]}")
-            elif file_snapshot.extension == "py":
-                print(f"Line Count: {file_snapshot.line_count}")
-                print(f"Class Count: {file_snapshot.class_count}")
-                print(f"Method Count: {file_snapshot.method_count}")
-            print(f"Created at: {file_snapshot.created_time}")
-            print(f"Last Updated at: {file_snapshot.updated_time}")
+        if filename in self.files:
+            file = self.files[filename]
+            if filename.lower().endswith('.txt'):
+                return TextFile(filename, file.path).get_file_info()
+            elif filename.lower().endswith(('.png', '.jpg')):
+                return ImageFile(filename, file.path).get_file_info()
+            elif filename.lower().endswith(('.py', '.java')):
+                return ProgramFile(filename, file.path).get_file_info()
+            else:
+                return file.get_file_info()
         else:
-            print(f"File not found: {filename}")
+            return "File not found."
 
     def status(self):
-        if self.snapshot_time is None:
-            print("Snapshot not taken. Please commit a snapshot first.")
-        else:
-            for filename, file_snapshot in self.file_snapshots.items():
-                status = "No Change" if file_snapshot.updated_time is not None and file_snapshot.updated_time <= self.snapshot_time else "Changed"
-                print(f"{filename}: {status}")
-
-    def update_snapshots(self):
-        current_files = os.listdir(self.folder_path)
-        for filename in current_files:
-            if filename not in self.file_snapshots:
-                file_extension = filename.split('.')[-1]
-                if file_extension == "txt":
-                    self.file_snapshots[filename] = TextFileSnapshot(filename)
-                elif file_extension == "png":
-                    self.file_snapshots[filename] = ImageFileSnapshot(filename)
-                elif file_extension == "py":
-                    self.file_snapshots[filename] = ProgramFileSnapshot(filename)
-                else:
-                    self.file_snapshots[filename] = FileSnapshot(filename, file_extension)
-
-            if filename.endswith(".txt"):
-                with open(os.path.join(self.folder_path, filename), "r") as file:
-                    self.file_snapshots[filename].line_count = len(file.readlines())
-                    file.seek(0)
-                    self.file_snapshots[filename].word_count = len(file.read().split())
-                    file.seek(0)
-                    self.file_snapshots[filename].character_count = len(file.read())
-
-            if filename.endswith(".png"):
-                image_path = os.path.join(self.folder_path, filename)
-                image_size = os.path.getsize(image_path)
-                self.file_snapshots[filename].image_size = (image_size, image_size)
-
-            if filename.endswith(".py"):
-                with open(os.path.join(self.folder_path, filename), "r") as file:
-                    lines = file.readlines()
-                    self.file_snapshots[filename].line_count = len(lines)
-                    self.file_snapshots[filename].class_count = sum(1 for line in lines if "class" in line)
-                    self.file_snapshots[filename].method_count = sum(1 for line in lines if "def" in line)
-
-class Main:
-    def __init__(self, path):
-        self.path = path
-
-    def run(self):
-        file_monitor = FileMonitor(self.path)
-
-        while True:
-            command = input("Enter a command (commit/info <filename>/status/exit): ").split()
-
-            if command[0] == "commit":
-                file_monitor.commit()
-                print("Snapshot updated.")
-            elif command[0] == "info":
-                if len(command) == 2:
-                    file_monitor.info(command[1])
-            elif command[0] == "status":
-                file_monitor.update_snapshots()
-                file_monitor.status()
-            elif command[0] == "exit":
-                break
-            else:
-                print("Invalid command. Please try again.")
+        for filename, file in self.files.items():
+            print(file.get_status())
 
 if __name__ == "__main__":
-    main = Main("E:\programs\OOP Labs\Laboratory-works-OOP\control")
-    main.run()
+    folder_path = "E:\programs\OOP Labs\Laboratory-works-OOP\control"
+    monitor = FolderMonitor(folder_path)
+
+    while True:
+        command = input("Enter command (commit, info <filename>, status): ").split()
+        if command[0] == "commit":
+            monitor.commit()
+        elif command[0] == "info":
+            if len(command) == 2:
+                filename = command[1]
+                result = monitor.info(filename)
+                print(result)
+            else:
+                print("Invalid command. Usage: info <filename>")
+        elif command[0] == "status":
+            monitor.status()
+        else:
+            print("Invalid command.")
